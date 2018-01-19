83f23d25fc12a134dc4ae3b94f8cc5298d6570d3
Ivan Valentinov <ivan.valentinov@pccomponentes.com>
2018-01-19 12:42:47 +0100

    GRID 2

--- src/components/grid/gridExample-01.js
+++ src/components/grid/gridExample-01.js
@@ -5,21 +5,29 @@ import { Row, Col, Container } from 'react-grid-system';
 const Brick = styled.div`
 	background: #000;
 	color: #fff;
+
+	img {
+		width: 100%;
+		height: 100%;
+		max-width: 250px;
+		max-height: 250px;
+	}
 `;
 
 const GridExample = () => (
 	<Container fluid>
 		<Row nogutter>
-			<Col sm={3}>
+			<Col sm={3} md={4} lg={6}>
 				<Brick>
-					Div LEFT
+					Div 1
+					<img src="http://via.placeholder.com/250x250" alt="" />
 				</Brick>
 			</Col>
-			<Col sm={6} align="end" debug>
+			<Col sm={6} md={4} lg={6} align="end" debug>
 				CENTER
 			</Col>
-			<Col sm={3} debug>
-				Div Right
+			<Col sm={3} md={4} lg={12} debug>
+				Div 2
 			</Col>
 		</Row>
 	</Container>
