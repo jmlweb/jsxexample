af2d3fc0d072965543b37e729b108b36394dbc0d
Ivan Valentinov <ivan.valentinov@pccomponentes.com>
2018-01-26 12:12:57 +0100

    Grid experiments 3

--- src/components/gridThoughts/gridThoughts.js
+++ src/components/gridThoughts/gridThoughts.js
@@ -4,27 +4,28 @@ import { Grid, Cell } from 'styled-css-grid';
 
 const Brick = styled.div`
   height:     100%;
-  border:     #333 1px solid; 
+  /* border:     #333 1px solid;  */
+  background-color: #00000036;
 `;
 
 const HeaderContent = styled.div`
   background: #d2f4c6;
-  padding: 8px;
+  padding:    8px;
 `;
 
 const MainContent = styled.div`
   background: #e9edf8;
-  padding:8px;
+  padding:    8px;
 `;
 
 const FooterContent = styled.div`
   background: #f9b1fa;
-  padding:8px;
+  padding:    8px;
 `;
 
 const Card = styled.div`
   background: #c2c2c2;
-  border: #444 1px solid;
+  border:     #444 1px solid;
 `;
 
 const GridThoughts = () => (
@@ -89,7 +90,7 @@ const GridThoughts = () => (
       gap="0"
       flow="row" //not required
       columns={"20% 1fr"}
-      rows={"minmax(55px,auto) 1fr minmax(55px,auto)"}>
+      rows={"minmax(auto,auto) auto minmax(55px,auto)"}>
       <Cell center width={2}>
         <HeaderContent>
           Header
@@ -100,6 +101,13 @@ const GridThoughts = () => (
       <Cell>
         <MainContent>
           Main Content here
+          <Grid flow="column" columns={3}>
+            <Cell height={1}><Brick>Div Content 1</Brick></Cell>
+            <Cell height={2}><Brick>Div Content 2</Brick></Cell>
+            <Cell height={4}><Brick>Div Content 3</Brick></Cell>
+            <Cell height={1}><Brick>Div Content 4</Brick></Cell>
+            <Cell height={5}><Brick>Div Content 5</Brick></Cell>
+          </Grid>
         </MainContent>
       </Cell>
 
