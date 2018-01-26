10bfd600c88464307e825486ad788b3a15f234bd
Ivan Valentinov <ivan.valentinov@pccomponentes.com>
2018-01-26 15:47:40 +0100

    Grid experiments 6

--- src/components/gridThoughts/gridThoughts.js
+++ src/components/gridThoughts/gridThoughts.js
@@ -4,8 +4,9 @@ import { withBreakpoints } from 'react-breakpoints';
 import { Grid, Cell } from 'styled-css-grid';
 import { BreakPoints } from '../../constants/breakpoints';
 
-const MAIN_WIDTH = '20%';
-const LESS_COLUMNS = 2;
+const OJETE_WIDTH = '20%';
+const MORENO_WIDTH = '100%';
+const FEW_COLUMNS = 2;
 const ALL_COLUMNS = 3;
 
 const Brick = styled.div`
@@ -34,104 +35,120 @@ const Card = styled.div`
   border:     #444 1px solid;
 `;
 
-const GridThoughts = props => (
+const GridThoughts = (props) => {
+  const isBig = props.currentBreakpoint > BreakPoints.LG;
+  const cssProps = isBig
+    ? ({
+      columns: `${OJETE_WIDTH} 1fr`,
+    })
+    : ({
+      columns: MORENO_WIDTH,
+    });
 
+  return (
+    <section>
 
-  <section>
-
-    <Grid
-      gap="0px"
-      columns={"100px 1fr 100px"}
-      rows="auro 1fr auto"
-      areas={[
-        "header header  header",
-        "menu   content ads   ",
-        "footer footer  footer",
-      ]}>
-      <Cell center area="header">
-        <HeaderContent>
-          Header
+      <Grid
+        gap="0px"
+        columns={"100px 1fr 100px"}
+        rows="auro 1fr auto"
+        areas={[
+          "header header  header",
+          "menu   content ads   ",
+          "footer footer  footer",
+        ]}>
+        <Cell center area="header">
+          <HeaderContent>
+            Header
         </HeaderContent>
-      </Cell>
+        </Cell>
 
-      <Cell area="content">
-        <MainContent>
-          Main Content
+        <Cell area="content">
+          <MainContent>
+            Main Content
 
           <Grid columns="repeat(auto-fit, minmax(120px, 1fr))" minRowHeight="auto" className="oops">
-            {
-              [...Array(18)].map((_obj, idx) => (
-                <Cell middle center key={idx}>
-                  <Card>Card Content</Card>
-                </Cell>
-              ))
-            }
-          </Grid>
+              {
+                [...Array(18)].map((_obj, idx) => (
+                  <Cell middle center key={idx}>
+                    <Card>Card Content</Card>
+                  </Cell>
+                ))
+              }
+            </Grid>
 
-        </MainContent>
-      </Cell>
+          </MainContent>
+        </Cell>
 
-      <Cell area="menu">
-        <Brick>
-          Menu
+        <Cell area="menu">
+          <Brick>
+            Menu
         </Brick>
-      </Cell>
+        </Cell>
 
-      <Cell area="ads">
-        <Brick>
-          Ads content
+        <Cell area="ads">
+          <Brick>
+            Ads content
         </Brick>
-      </Cell>
+        </Cell>
 
-      <Cell center area="footer">
-        <FooterContent>
-          Footer Content
+        <Cell center area="footer">
+          <FooterContent>
+            Footer Content
         </FooterContent>
-      </Cell>
-    </Grid>
+        </Cell>
+      </Grid>
 
-    <p>&nbsp;</p>
+      <p>&nbsp;</p>
 
-    <Grid
-      gap="0"
-      flow="row" //not required
-      columns={`${MAIN_WIDTH} 1fr`}
-      rows={"minmax(auto,auto) auto minmax(55px,auto)"}>
-      <Cell center width={2}>
-        <HeaderContent>
-          Header
-        </HeaderContent>
-      </Cell>
+      <Grid
+        gap='0'
+        flow='row' //not required
+        rows='minmax(auto,auto) auto minmax(55px,auto)'
+        {...cssProps}
+      >
 
-      <Cell middle>Menu</Cell>
-      <Cell>
-        <MainContent>
-          Main Content
+        <Cell center width={2}>
+          <HeaderContent>
+            Header
+          </HeaderContent>
+        </Cell>
+
+        {isBig
+          ? <Cell middle>Menu</Cell>
+          : undefined
+        }
+
+        {/* <Cell middle>Menu</Cell> */}
+
+        <Cell>
+          <MainContent>
+            Main Content
           {/* flow-column */}
-          <Grid flow="row dense" columns={props.currentBreakpoint < BreakPoints.LG ? LESS_COLUMNS : ALL_COLUMNS}>
-            <Cell height={1}><Brick>Div--1</Brick></Cell>
-            <Cell height={2}><Brick>Div--2</Brick></Cell>
-            <Cell height={4}><Brick>Div--3</Brick></Cell>
-            <Cell height={1}><Brick>Div--4</Brick></Cell>
-            <Cell height={5}><Brick>Div--5</Brick></Cell>
-            <Cell height={2}><Brick>Div--6</Brick></Cell>
-            <Cell height={1}><Brick>Div--7</Brick></Cell>
-            <Cell height={1}><Brick>Div--8</Brick></Cell>
-            <Cell height={2}><Brick>Div--9</Brick></Cell>
-            <Cell height={2}><Brick>Div--10</Brick></Cell>
-          </Grid>
-        </MainContent>
-      </Cell>
+            <Grid flow="row dense" columns={props.currentBreakpoint < BreakPoints.XL ? FEW_COLUMNS : ALL_COLUMNS}>
+              <Cell height={1}><Brick>Div--1</Brick></Cell>
+              <Cell height={2}><Brick>Div--2</Brick></Cell>
+              <Cell height={4}><Brick>Div--3</Brick></Cell>
+              <Cell height={1}><Brick>Div--4</Brick></Cell>
+              <Cell height={5}><Brick>Div--5</Brick></Cell>
+              <Cell height={2}><Brick>Div--6</Brick></Cell>
+              <Cell height={1}><Brick>Div--7</Brick></Cell>
+              <Cell height={1}><Brick>Div--8</Brick></Cell>
+              <Cell height={2}><Brick>Div--9</Brick></Cell>
+              <Cell height={2}><Brick>Div--10</Brick></Cell>
+            </Grid>
+          </MainContent>
+        </Cell>
 
-      <Cell center width={2}>
-        <FooterContent>
-          Footer
+        <Cell center width={2}>
+          <FooterContent>
+            Footer
         </FooterContent>
-      </Cell>
-    </Grid>
+        </Cell>
+      </Grid>
 
-  </section >
-
-);
+    </section>
+  )
+};
 
 export default withBreakpoints(GridThoughts);
